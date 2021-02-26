1d7c886f0de042e07e4f6638fa617e9085cc56d6
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2021-02-26 23:05:12 +0900

    Loop

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,25 @@ use std::io;
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 100 + 1);
     println!("The Secret Number is {}", secret_number);
+    loop {
+        let mut number = String::new();
+        println!("Guess A Number:");
 
-    let mut number = String::new();
-    println!("Guess A Number:");
+        io::stdin()
+            .read_line(&mut number)
+            .expect("Failed to Read Line");
 
-    io::stdin()
-        .read_line(&mut number)
-        .expect("Failed to Read Line");
+        // * Rust allows us to shadow the previous value with a new one
+        let number: u32 = number.trim().parse().expect("Please Type a Number!");
+        println!("You Guessed : {}", number);
 
-    // * Rust allows us to shadow the previous value with a new one
-    let number: u32 = number.trim().parse().expect("Please Type a Number!");
-    println!("You Guessed : {}", number);
-
-    match number.cmp(&secret_number) {
-        Ordering::Less => println!("It's Small."),
-        Ordering::Equal => println!("Great, You're a Winner."),
-        Ordering::Greater => println!("It's Big."),
+        match number.cmp(&secret_number) {
+            Ordering::Less => println!("It's Small."),
+            Ordering::Equal => {
+                println!("Great, You're a Winner.");
+                break;
+            }
+            Ordering::Greater => println!("It's Big."),
+        }
     }
 }
