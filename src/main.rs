7305d4bf828f2ff5208ec1a84326e45be6546c71
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2021-02-27 18:17:54 +0900

    Shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,8 @@
-extern crate rand;
-
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
-
 fn main() {
-    let secret_number = rand::thread_rng().gen_range(1, 100 + 1);
-    println!("The Secret Number is {}", secret_number);
-    loop {
-        let mut number = String::new();
-        println!("Guess A Number:");
-
-        io::stdin()
-            .read_line(&mut number)
-            .expect("Failed to Read Line");
-
-        // * Rust allows us to shadow the previous value with a new one
-        let number: u32 = match number.trim().parse() {
-            Ok(num) => num,
-            Err(e) => {
-                println!("Error: {}", e);
-                continue;
-            }
-        };
-        println!("You Guessed : {}", number);
-
-        match number.cmp(&secret_number) {
-            Ordering::Less => println!("It's Small."),
-            Ordering::Equal => {
-                println!("Great, You're a Winner.");
-                break;
-            }
-            Ordering::Greater => println!("It's Big."),
-        }
-    }
+    let x = 1;
+    println!("x: {}", x); // * 1
+    let x = 2;
+    println!("x: {}", x); // * 2
+    let x = 3;
+    println!("x: {}", x); // * 3
 }
