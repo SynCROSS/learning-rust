37cb493a9ee927f934d0072a578c80da5a3bb2cc
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2021-03-01 18:54:36 +0900

    Fibonacci

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,35 @@
 use std::io::{self, Write};
 
 fn main() {
-    loop {
-        let mut input = String::new();
-        println!("-------------------------");
-        println!("(1) Celsius to Fahrenheit");
-        println!("(2) Fahrenheit to Celsius");
-        println!("-------------------------");
-        print!("Input: ");
-        io::stdout().flush().unwrap();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Please Type a Number");
-
-        let mut number = String::new();
-
-        let input: i32 = match input.trim().parse() {
-            Ok(num) => num,
-            Err(e) => {
-                println!("Error: {}", e);
-                continue;
-            }
-        };
-
-        if input == 1 {
-            print!("Celsius: ");
-            io::stdout().flush().unwrap();
-            io::stdin()
-                .read_line(&mut number)
-                .expect("Please Type a Number");
-
-            let celsius: f64 = number.trim().parse().unwrap();
-            println!(
-                "Celsius {} to Fahrenheit is {}",
-                celsius,
-                make_celsius_to_fahrenheit(celsius)
-            );
-        } else if input == 2 {
-            print!("Fahrenheit: ");
-            io::stdout().flush().unwrap();
-            io::stdin()
-                .read_line(&mut number)
-                .expect("Please Type a Number");
-
-            let fahrenheit: f64 = number.trim().parse().unwrap();
-            println!(
-                "Fahrenheit {} to Celsius is {}",
-                fahrenheit,
-                make_fahrenheit_to_celsius(fahrenheit)
-            );
-        } else {
-            break;
+    print!("Length: ");
+    io::stdout().flush().unwrap();
+    let mut input2 = String::new();
+    io::stdin()
+        .read_line(&mut input2)
+        .expect("Error has Occurred.");
+    let length = match input2.trim().parse() {
+        Ok(num) => num,
+        Err(e) => {
+            println!("Error: {}", e);
+            0
         }
+    };
+
+    fibonacci(length);
+}
+
+fn fibonacci(num: i64) {
+    let mut pre_tmp = 1;
+    let mut tmp = 1;
+    let mut i = 0;
+
+    loop {
+        i = tmp + pre_tmp;
+        println!("{} {} {}", pre_tmp, tmp, i);
+        pre_tmp = tmp;
+        tmp = i;
+        if i > num {
+            break;
+        };
     }
-}
-
-fn make_celsius_to_fahrenheit(celsius: f64) -> f64 {
-    let fahrenheit: f64 = celsius * 1.8 + 32.0;
-    fahrenheit
-}
-
-fn make_fahrenheit_to_celsius(fahrenheit: f64) -> f64 {
-    let celsius: f64 = (fahrenheit - 32.0) / 1.8;
-    celsius
 }
