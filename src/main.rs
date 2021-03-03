6395bd0b57f817e248cd71512522e2cf95600f0a
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2021-03-03 21:42:09 +0900

    About Copy Trait

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 fn main() {
+    // * Except For the Rust Primitive Types,
+    // * The Rest(Vec, String, etc) Have NO Copy Trait.
+    // * Copy Trait is Copyable Trait.
+
     let v = vec![1, 2, 3];
 
-    let v2 = v;
+    let v2 = &v; // * However, if it was assigned an reference, there is no error.
 
-    println!("v[0] is: {}", v[0]); // ! Error, Because v's ownership moved to v2;
+    println!("v[0] is: {}", v[0]); // * v[0] is: 1
 }
