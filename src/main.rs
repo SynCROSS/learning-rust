48ff001ecdf2aa46e0056db5c44f56638273e665
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-03-16 22:13:09 +0900

    Mutable References

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 fn main() {
-    let s1 = String::from("hello");
+    let mut s = String::from("hello");
 
-    let len = calculate_length(&s1);
-
-    println!("The length of '{}' is {}.", s1, len);
+    println!("{}", change(&mut s));
 }
 
-fn calculate_length(s: &String) -> usize {
-    // * s is a reference to a String
-    s.len()
-} // * Here, s goes out of scope.
-  // * But because it does not have ownership of
-  // * what it refers to, nothing happens.
+// * Mutable References
+fn change(some_string: &mut String) -> &mut String {
+    some_string.push_str(", world");
+
+    some_string
+}
