5efea4e46ba645fb7ad8c03d9400d2f0578b75f3
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-03-21 19:25:02 +0900

    Format without Prettier

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 fn main() {
-	let mut s = String::from("hello");
+    let mut s = String::from("hello");
 
-	println!("{}", change(&mut s));
+    println!("{}", change(&mut s));
 }
 
 // * Mutable References
 fn change(some_string: &mut String) -> &mut String {
-	some_string.push_str(", world");
+    some_string.push_str(", world");
 
-	some_string
+    some_string
 }
