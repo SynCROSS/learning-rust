9b595a39d1943d747508f5782d872a2bfc0d8aad
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-03-21 20:50:18 +0900

    Prevent Data Race

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 fn main() {
+	// * But mutable references have one big restriction: 
+	// * you can have only one mutable reference to 
+	// * a particular piece of data in a particular scope.
+
+	// * This restriction allows for mutation but in a very controlled fashion. 
+	// * It’s something that a man stranger to Rust struggle with, 
+	// * because most languages let you mutate whenever you’d like.
+
+	// * The benefit of having this restriction is that Rust can prevent data races at compile time. 
+	// * A data race is similar to a race condition and happens when these three behaviors occur:
+
+	// * Two or more pointers access the same data at the same time.
+	// * At least one of the pointers is being used to write to the data.
+	// * There’s no mechanism being used to synchronize access to the data.
+    
     let mut s = String::from("hello");
 
-    println!("{}", change(&mut s));
-}
+    let r1 = &mut s;
+    let r2 = &mut s;
 
-// * Mutable References
-fn change(some_string: &mut String) -> &mut String {
-    some_string.push_str(", world");
-
-    some_string
+    println!("{}, {}", r1, r2);
 }
