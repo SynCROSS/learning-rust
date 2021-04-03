9eb7f0ee0c9d14c0758972fdfd047e92d4e30449
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-04-03 22:17:05 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,34 @@
+// * If we have a string slice, we can pass that directly.
+// * If we have a String, we can pass a slice of the entire String.
+// * Defining a function to take a string slice instead of a reference
+// * to a String makes our API more general and useful without losing any functionality
+
 fn main() {
-    // *  A string slice is a reference to part of a String, and it looks like this:
-    let s = String::from("hello world");
+    let my_string = String::from("hello world");
 
-    let hello = &s[0..5];
-    let world = &s[6..11];
+    // * first_word works on slices of `String`s
+    let word = first_word(&my_string[..]);
+    println!("{}", word);
 
-    println!("{} {}", hello, world);
+    let my_string_literal = "hello world";
+    // * first_word works on slices of string literals
+    let word = first_word(&my_string_literal[..]);
+    println!("{}", word);
+
+    // * Because string literals *are* string slices already,
+    // * this works too, without the slice syntax!
+    let word = first_word(my_string_literal);
+    println!("{}", word);
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
