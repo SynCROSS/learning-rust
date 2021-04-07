8466ed527a089f1104f5d5434e1e246f905f325f
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-04-07 21:35:41 +0900

    Strruct

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 fn main() {
-    let a = [1, 2, 3, 4, 5];
+    struct Person {
+        name: String,
+        age: i8,
+    }
 
-    let slice = &a[1..3];
+    let person1 = Person {
+        name: String::from("SynCROSS"),
+        age: 18,
+    };
 
-    println!("{:?}", slice);
+    println!("Name: {}", person1.name);
+    println!("Age: {}", person1.age)
 }
