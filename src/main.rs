1a2808bbe5c8cf77b26517d6c74f4b730438e438
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-04-11 20:15:04 +0900

    Access Elements of Tuple Structs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-#![allow(unused)]
+// #![allow(unused)]
 fn main() {
     struct Color(i32, i32, i32);
-    struct Point(i32, i32, i32);
-
     let black = Color(0, 0, 0);
-    let origin = Point(0, 0, 0);
+    println!("Red: {}, Green: {}, Blue: {}", &black.0, &black.1, &black.2)
 }
