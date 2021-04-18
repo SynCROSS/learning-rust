14df6e6ba91483835b0205e73cd7d76756a592d0
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-04-18 23:29:36 +0900

    pub Keyword

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,18 +1,24 @@
-// * Absolute path: It starts from a crate root by using a crate name or a literal crate.
-// * Relative path: It starts from the current module and uses self, super, or an identifier in the current module.
-
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-    }
-
-    mod serving {
+    // snip
+    pub mod serving {
         fn take_order() {}
 
-        fn serve_order() {}
+        pub fn serve_order() {}
 
         fn take_payment() {}
     }
+}
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::front_of_house::serving::serve_order();
+    }
+
+    fn cook_order() {}
+}
+
+pub fn eat_at_restaurant() {
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    front_of_house::hosting::add_to_waitlist();
 }
