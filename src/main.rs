d9947f9ef9ebecef0de09da637951a114987bb90
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-05-05 19:56:28 +0900

    Overwriting Hash Map

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
 fn main() {
-    let field_name = String::from("Favorite color");
-    let field_value = String::from("Blue");
+    let mut scores = std::collections::HashMap::new();
 
-    let mut map = std::collections::HashMap::new();
-    map.insert(field_name, field_value);
-    // * field_name and field_value are invalid at this point, try using them and
-    // * see what compiler error you get!
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 25);
 
-    for (key, value) in &map {
-        println!("{}: {}", key, value);
-    }
+    println!("{:?}", scores);
 }
