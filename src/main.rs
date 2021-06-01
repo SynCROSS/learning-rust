3f04e8b845ea843df7eabf5ab5b2bb7cf6346999
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-06-01 23:34:58 +0900

    Struct Definitions with Generics

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,12 @@
-fn largest_i32(list: &[i32]) -> i32 {
-    let mut largest = list[0];
-
-    for &item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
-}
-
-fn largest_char(list: &[char]) -> char {
-    let mut largest = list[0];
-
-    for &item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
+struct Point<T> {
+    x: T,
+    y: T,
 }
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
 
-    let result = largest_i32(&number_list);
-    println!("The largest number is {}", result);
-    assert_eq!(result, 100);
-
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest_char(&char_list);
-    println!("The largest char is {}", result);
-    assert_eq!(result, 'y');
+    println!("( {:?}, {:?} )", &integer.x, &integer.y);
+    println!("( {:?}, {:?} )", &float.x, &float.y);
 }
