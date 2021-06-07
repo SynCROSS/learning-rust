2893a0f81142e7cfccb889b74675ac08ea4832f9
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-06-07 21:03:11 +0900

    Integer and Float Are NOT Same

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-struct Point<T> {
+struct Point<T, U> {
     x: T,
-    y: T,
+    y: U,
 }
 
 fn main() {
-    let integer = Point { x: 5, y: 10 };
-    let float = Point { x: 1.0, y: 4.0 };
+    let both_integer = Point { x: 5, y: 10 };
+    let both_float = Point { x: 1.0, y: 4.0 };
+    let integer_and_float = Point { x: 5, y: 4.0 };
 
-    println!("( {:?}, {:?} )", &integer.x, &integer.y);
-    println!("( {:?}, {:?} )", &float.x, &float.y);
+    println!("( {:?}, {:?} )", both_integer.x, both_integer.y);
+    println!("( {:?}, {:?} )", both_float.x, both_float.y);
+    println!("( {:?}, {:?} )", integer_and_float.x, integer_and_float.y);
 }
