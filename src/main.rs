1d4dfe345eefdea9d7c860a7af349ec292a9ae86
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-07-02 22:02:15 +0900

    Implement & Call Methods

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-pub trait Summary {
-    fn summarize(&self) -> String;
+use learning_rust::{Summary, Tweet};
+
+fn main() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
 }
