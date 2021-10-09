697c576775c0929c8805ae4b70fe0926d35d9929
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-10-09 09:49:04 +0900

    Returning either a NewsArticle or a Tweet isn’t allowed

--- src/lib.rs
+++ src/lib.rs
@@ -37,16 +37,17 @@ fn returns_summarizable(switch: bool) -> impl Summary {
         NewsArticle {
             headline: String::from("Penguins win the Stanley Cup Championship!"),
             location: String::from("Pittsburgh, PA, USA"),
-            author: String::from("Ice burgh"),
+            author: String::from("Iceburgh"),
             content: String::from(
                 "The Pittsburgh Penguins once again are the best hockey team in the NHL.",
             ),
         }
-    }
-    Tweet {
-        username: String::from("horse_ebooks"),
-        content: String::from("of course, as you probably already know, people"),
-        reply: false,
-        retweet: false,
+    } else {
+        Tweet {
+            username: String::from("horse_ebooks"),
+            content: String::from("of course, as you probably already know, people"),
+            reply: false,
+            retweet: false,
+        }
     }
 }
